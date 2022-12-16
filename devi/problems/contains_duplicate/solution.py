92dc5e8b879825bf559df691e56ecd6892e47e27
devi krishna <66028354+devxkrxshna@users.noreply.github.com>
2022-12-16 09:02:23 +0000

    Sync LeetCode submission - Contains Duplicate (python3)

--- devi/problems/contains_duplicate/solution.py
+++ devi/problems/contains_duplicate/solution.py
@@ -1,9 +1,11 @@
-class Solution(object):
-    def containsDuplicate(self, arr):
-        dictt={}
-        for i in arr:
-            if i not in dictt:
-                dictt[i]=1
-            else:
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        items={}
+        for i in nums:
+            items[i]=items.get(i,0)+1
+        print(items)
+        for val in items.values():
+            if val>1:
                 return True
         return False
+                
