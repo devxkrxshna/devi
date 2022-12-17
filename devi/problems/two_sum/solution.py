48bdff488b65735bafa3c442257eadde80f5df57
devi krishna <66028354+devxkrxshna@users.noreply.github.com>
2022-12-17 10:03:49 +0000

    Sync LeetCode submission - Two Sum (python3)

--- devi/problems/two_sum/solution.py
+++ devi/problems/two_sum/solution.py
@@ -1,9 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d = {}
-        for i, n in enumerate(nums):
-            m = target - n
-            if m in d:
-                return [d[m], i]
+        hasht={}
+        for i,num in enumerate(nums):
+            if target-num in hasht:
+                return [hasht[target-num],i]
             else:
-                d[n] = i
+                hasht[num]=i
+        return [-1,-1]
